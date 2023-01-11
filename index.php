f985934f0799c0a3e8ebb22d588ef16199b5d055
DiabateJS <diabatejs@yahoo.fr>
2023-01-11 10:51:29 +0100

    Intégration Bootstrap CDN.

--- index.php
+++ index.php
@@ -8,24 +8,32 @@ include_once "articles.model.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Vente en ligne</title>
 </head>
 <body> 
+<div class="container">
+<br>
+<?php
+include "entete.php";
+?>
+<br>
 <div class="articles">
 <?php
 for ($i = 0 ; $i < count($articles) ; $i++){ 
 ?>
-    <div class="article">
-        Id :  <?= $articles[$i]->id ?><br>
-        Image : <?= $articles[$i]->img_link ?><br>
-        <h3><b><?= $articles[$i]->libelle ?></b></h3>
-        <i><?= $articles[$i]->prix ?> F CFA</i><br>
-        <br>
-        Disponible <br>
-        Quantité : <?= $articles[$i]->qte ?><br>
-        Commentaire : <?= $articles[$i]->commentaire ?> <br>
-        <br>
-        <button>Commander</button>
+    <div class="card article" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?= $articles[$i]->libelle ?></h5>
+            <p class="card-text">
+                Id :  <?= $articles[$i]->id ?><br>
+                Image : <?= $articles[$i]->img_link ?><br>
+                Quantité : <?= $articles[$i]->qte ?><br>
+                Commentaire : <?= $articles[$i]->commentaire ?> <br>
+            </p>
+            <a href="#" class="btn btn-primary">Commander</a>
+        </div>
     </div>
 <?php
 }
@@ -33,5 +41,7 @@ for ($i = 0 ; $i < count($articles) ; $i++){
 </div>
 <br>
 <a href="nouveau.php">Nouveau</a>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
--- nouveau.php
+++ nouveau.php
@@ -4,23 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Nouvel article</title>
 </head>
 <body>
+<div class="container">
+<br>
+<?php
+include "entete.php";
+?>
+<br>
 <h3>Création article</h3>
 <br>
 <form method="POST" action="creation_article.php">
-    Libelle : <input type="text" name="libelle" /><br>
-    Prix : <input type="text" name="prix" /><br>
-    Quantité : <input type="text" name="qte" /><br>
-    Image : <input type="text" name="img_link" /><br>
-    Commentaire : <br>
-    <textarea></textarea><br>
-    <input type="submit" value="Enregistrer" />
-    <button>Nouveau</button>
+    <div class="mb-3">
+        <label for="libelle" class="form-label">Libelle</label>
+        <input type="text" class="form-control" name="libelle" id="libelle">
+    </div>
+    <div class="mb-3">
+        <label for="prix" class="form-label">Prix</label>
+        <input type="text" class="form-control" name="prix" id="prix">
+    </div>
+    <div class="mb-3">
+        <label for="qte" class="form-label">Quantité</label>
+        <input type="text" class="form-control" name="qte" id="qte">
+    </div>
+    <div class="mb-3">
+        <label for="formFile" class="form-label">Image article</label>
+        <input class="form-control" type="file" name="img_link" id="formFile">
+    </div>
+    <div class="mb-3">
+        <label for="commentaire" class="form-label">Commentaire</label>
+        <textarea class="form-control" id="commentaire" name="commentaire" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Enregistrer</button>
+    <button class="btn btn-success">Nouveau</button>
 </form>
 <br>
-<br>
 <a href="index.php">Articles</a>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </html>
