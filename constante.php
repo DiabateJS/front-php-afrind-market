d8f03c8b67e584818a22877e2d1b03ea072c8a06
DiabateJS <diabatejs@yahoo.fr>
2023-02-02 15:40:12 +0100

    Parametres de prod

--- constante.php
+++ constante.php
@@ -1,10 +1,10 @@
 <?php
 class Constante {
     public static $LOCAL_BD_CONFIG = array(
-        "host" => "localhost",
-        "user" => "root",
-        "password" => "root",
-        "bdname" => "afrind_market"
+        "host" => "185.98.131.90",
+        "user" => "djste1070339",
+        "password" => "da6ysjpqpp",
+        "bdname" => "djste1070339"
     );
 }
 ?>
