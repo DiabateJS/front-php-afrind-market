ffbfc7f1554e686752b4265ea8beea88e39c5f3e
DiabateJS <diabatejs@yahoo.fr>
2023-02-03 14:40:59 +0100

    Mise à jour des paramétres de prod.

--- constante.php
+++ constante.php
@@ -1,12 +1,12 @@
 <?php
 class Constante {
     public static $LOCAL_BD_CONFIG = array(
-        "host" => "localhost",
-        "user" => "root",
-        "password" => "root",
-        "bdname" => "afrind_market"
+        "host" => "185.98.131.90",
+        "user" => "djste1070339",
+        "password" => "da6ysjpqpp",
+        "bdname" => "djste1070339"
     );
 
-    public static $SELECT_ARTICLES = "select id, libelle, qte, prix, img_link, commentaire from article";
+    public static $SELECT_ARTICLES = "select id, libelle, qte, prix, img_link, commentaire from am_articles";
 }
 ?>
